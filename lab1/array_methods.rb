1ad585885604962194ec1af7acfc34457912a122
globalwa <sir@DESKTOP-CJNV3F3>
2024-09-14 01:54:41 +0300

    feat(lab1/array_methods): add process_array to run method selected by user with reading data from provided file

--- lab1/array_methods.rb
+++ lab1/array_methods.rb
@@ -21,3 +21,28 @@ def get_first_positive_number_index(numbers)
 
 	nil
 end
+
+def process_array(selected_method, path)
+  file = File.new(path)
+
+  array = file.readlines.map{|line| line.chomp}.map{|value| value.to_i}
+
+  method_result = nil
+
+  case selected_method
+  when 1
+    method_result = get_min(array)
+  when 2
+    method_result = get_first_positive_number_index(array)
+  end
+
+  method_result
+end
+
+puts "введите метод: \n1 - найти минимальный элемент массива\n2 - найти первый положительный элемент\n"
+selected_method = gets.to_i
+
+puts "введите адрес файла где описан массив: "
+path = STDIN.gets.chomp
+
+puts process_array(selected_method, path)
